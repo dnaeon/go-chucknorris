d9603cf4fa33762c0866825f6a2df3efe83f5f19
Marin Atanasov Nikolov <dnaeon@gmail.com>
2017-03-02 10:41:16 +0200

    Initial commit of the cli commands

---
+++ cmd/categories.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/dnaeon/go-chucknorris/api"
+	"github.com/urfave/cli"
+)
+
+// NewCategoriesCmd creates a new command for retrieving the
+// Chuck Norris jokes category list.
+func NewCategoriesCommand() cli.Command {
+	cmd := cli.Command{
+		Name:   "categories",
+		Usage:  "shows the list of joke categories",
+		Action: execCategoriesCommand,
+	}
+
+	return cmd
+}
+
+func execCategoriesCommand(c *cli.Context) error {
+	client := api.NewClient(nil)
+	categories, _, err := client.Categories()
+
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	for _, category := range categories {
+		fmt.Println(category)
+	}
+
+	return nil
+}
---
+++ cmd/joke.go
@@ -0,0 +1,38 @@
+package cmd
+
+import (
+	"fmt"
+
+	"github.com/dnaeon/go-chucknorris/api"
+	"github.com/urfave/cli"
+)
+
+// NewJokeCommand creates a new command for retrieving a Chuck Norris joke.
+func NewJokeCommand() cli.Command {
+	cmd := cli.Command{
+		Name:   "joke",
+		Usage:  "tells a Chuck Norris joke",
+		Action: execJokeCommand,
+		Flags: []cli.Flag{
+			cli.StringFlag{
+				Name:  "category",
+				Usage: "choose a joke from given category",
+			},
+		},
+	}
+
+	return cmd
+}
+
+func execJokeCommand(c *cli.Context) error {
+	client := api.NewClient(nil)
+	category := api.Category(c.String("category"))
+	joke, _, err := client.RandomJoke(category)
+	if err != nil {
+		return cli.NewExitError(err.Error(), 1)
+	}
+
+	fmt.Println(joke.Value)
+
+	return nil
+}
